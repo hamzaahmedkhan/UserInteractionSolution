af25e2a8e01e454d686600b30aac2b4f9f09c98b
Hamza Khan <hamza_ahmed_khan@yahoo.com>
2020-01-06 00:12:43 +0500

    Initial Commit

---
+++ app/src/main/java/com/github/hamzaahmedkhan/userinteraction/DashboardActivity.java
@@ -0,0 +1,24 @@
+package com.github.hamzaahmedkhan.userinteraction;
+
+import android.os.Bundle;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import android.view.View;
+
+public class DashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+}
